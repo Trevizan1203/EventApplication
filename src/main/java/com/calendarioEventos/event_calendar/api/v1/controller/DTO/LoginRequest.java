65552b55ccce2be7914ad67f6b34acfdc0de6e31
Gustavo <gustavo-trevizan@hotmail.com>
2025-01-29 21:30:57 -0300

    feat: implementação de lógica e DTOs para login usando token jwt

---
+++ src/main/java/com/calendarioEventos/event_calendar/api/v1/controller/DTO/LoginRequest.java
@@ -0,0 +1,4 @@
+package com.calendarioEventos.event_calendar.api.v1.controller.DTO;
+
+public record LoginRequest(String username, String password) {
+}
---
+++ src/main/java/com/calendarioEventos/event_calendar/api/v1/controller/DTO/LoginResponse.java
@@ -0,0 +1,4 @@
+package com.calendarioEventos.event_calendar.api.v1.controller.DTO;
+
+public record LoginResponse(String accessToken, Long expiresIn) {
+}
