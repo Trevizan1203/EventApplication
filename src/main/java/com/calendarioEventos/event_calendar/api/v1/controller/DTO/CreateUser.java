15499e91195121dd46c36d02a74715e856199c08
Gustavo <gustavo-trevizan@hotmail.com>
2025-01-30 14:04:10 -0300

    feat: funcionalidade de cadastro de usuario

---
+++ src/main/java/com/calendarioEventos/event_calendar/api/v1/controller/DTO/CreateUser.java
@@ -0,0 +1,4 @@
+package com.calendarioEventos.event_calendar.api.v1.controller.DTO;
+
+public record CreateUser(String username, String password) {
+}
---
+++ src/main/java/com/calendarioEventos/event_calendar/services/UserService.java
@@ -0,0 +1,43 @@
+package com.calendarioEventos.event_calendar.services;
+
+import com.calendarioEventos.event_calendar.api.v1.controller.DTO.CreateUser;
+import com.calendarioEventos.event_calendar.entities.User;
+import com.calendarioEventos.event_calendar.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<Void> createUser(@RequestBody CreateUser dto) {
+
+        var userFromDB = userRepository.findByUsername(dto.username());
+
+        if (userFromDB.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Usuario ja existente");
+        }
+
+        User newUser = new User();
+        newUser.setUsername(dto.username());
+        newUser.setPassword(passwordEncoder.encode(dto.password()));
+
+        userRepository.save(newUser);
+
+        return ResponseEntity.ok().build();
+    }
+
+}
