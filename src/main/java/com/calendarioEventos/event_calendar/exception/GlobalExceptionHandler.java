7b2f73fce8351310e63ae8187ec2e8161c166d94
Gustavo <gustavo-trevizan@hotmail.com>
2025-01-29 21:31:40 -0300

    feat: inserção de classe para exceptions gerais

---
+++ src/main/java/com/calendarioEventos/event_calendar/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.calendarioEventos.event_calendar.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatusException(ResponseStatusException ex) {
+        return new ResponseEntity<>(ex.getMessage(), ex.getStatusCode());
+    }
+}
